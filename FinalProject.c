e694dc494c14d872ae02d83630128e6f3de9d9e2
Summers Wu <lwu259@uwo.ca>
2022-04-13 12:20:48 -0400

    canary branch

--- FinalProject.c
+++ FinalProject.c
@@ -1,79 +1,7 @@
 #include "address_map_arm.h"
 
-volatile int start;
-volatile int time;
-volatile int buttons;
-
-volatile int *hex0 = (int *)HEX3_HEX0_BASE;
-volatile int *hex1 = (int *)HEX5_HEX4_BASE;
-volatile int *buts = (int *)KEY_BASE;
-volatile int *timer = (int *)TIMER_BASE;
-// global variables
-volatile ADC * const adc_ptr = ( ADC *) ADC_BASE;
-volatile GPIO * const gpio_ptr = ( unsigned int *) JP1_BASE;
-
-// for the timer (IDK but it's from lab 1)
-int Exp(int x, int y) {
-    volatile int result = 1;
-    while (y != 0)
-    {
-        result *= x;
-        y--;
-    }
-    return result;
-}
-
-// DISPLAYS TIME
-void Display(int value) {
-    int decVals[10];
-    decVals[0] = 0x3F;
-    decVals[1] = 0x6;
-    decVals[2] = 0x5B;
-    decVals[3] = 0x4F;
-    decVals[4] = 0x66;
-    decVals[5] = 0x6D;
-    decVals[6] = 0x7D;
-    decVals[7] = 0x7;
-    decVals[8] = 0x7F;
-    decVals[9] = 0x6F;
-
-    volatile int i;
-    volatile int a = 0x0;
-    volatile int b = 0x0;
-    for (i = 5; i >= 0; i--) {
-        int digit = (value % Exp(10, i + 1)) / (Exp(10, i));
-
-        if (i > 3) {
-            b <<= 8;
-            b += decVals[digit];
-        }
-        else {
-            a <<= 8;
-            a += decVals[digit];
-        }
-    }
-    *(hex0) = a;
-    *(hex1) = b;
-    return;
-}
-
-// READ BUTTONS
-int ReadButs(void) {
-    return *(buts)&0xF;
-}
-
-struct Timer {
-    volatile int *status;
-    volatile int *control;
-    volatile int *loadL;
-    volatile int *loadH;
-    volatile int *readL;
-    volatile int *readH;
-};
-struct Timer *myTimer;
- 
-// structure for the GPIO
-//reads input, and lights up with respect to the voltage incrementing
+/* Our first program!  Watch the blinking light!
+*/
 typedef struct _GPIO {   
 	unsigned int data;   
 	unsigned int control;
@@ -90,128 +18,213 @@ typedef struct _ADC {
 	unsigned int ch7; 
 } ADC; 
 
-// READ VOLTAGE FROM POTENTIOMETER
-int readVoltage() {     
-	int voltage;            
-	// just reads 0-11 bits
-	voltage = adc_ptr->ch0 & 0xfff;     
+volatile GPIO * const gpio_ptr = ( unsigned int *) JP1_BASE;
+volatile ADC * const adc_ptr = ( ADC *) ADC_BASE;
+
+int readSwitches(void) {    
+	volatile unsigned int * switchPtr = (unsigned int *) SW_BASE;
+	volatile int switchInput;     
+	switchInput = *switchPtr & 0x1;     
+	return switchInput;
+}
+
+// read voltage from adc
+int readVoltage(num) {     
+	int voltage;     
+	if (num == 0){         
+		// just reads 0-11 bits
+		voltage = adc_ptr->ch0 & 0xfff;     
+	}     else if (num == 1) {         
+		voltage = adc_ptr->ch1 & 0xfff;     
+	}     
 	return voltage; 
 } 
 	
-// CONVERT VOLTAGE VALUE INTO AMOUNT OF TIME TO BE ADDED
-// as the dial is turned, more time is added to the count
-// set it to 13 so that there are 12 possible values (increases by 2 minutes/120 secs)
-int voltageToPercent(value) {     
-	int time = 0;     
+int voltageToPercent(value, normalized) {     
+	int ledOn;     
 	int voltAsPercent;
 	// 4096 = max voltage (2^12 bits)     
-	voltAsPercent = (value*13)/4096; 
+	voltAsPercent = (value*10)/normalized; 
 	
 	// 12 bit number, and reference voltage     
-	if (voltAsPercent == 1) {         
-		time = 10;    
-	}     else if (voltAsPercent == 2) {        
-		time = 20;   
+	if (voltAsPercent == 0) {         
+		ledOn = 0b0;     
+	}     else if (voltAsPercent == 1) {        
+		 ledOn = 0b1;     
+	}     else if (voltAsPercent == 2) {         
+		ledOn = 0b11;     
 	}     else if (voltAsPercent == 3) {         
-		time = 30;     
+		ledOn = 0b111;     
 	}     else if (voltAsPercent == 4) {         
-		time = 40;     
+		ledOn = 0b1111;     
 	}     else if (voltAsPercent == 5) {         
-		time = 50;    
+		ledOn = 0b11111;     
 	}     else if (voltAsPercent == 6) {         
-		time = 60;      
+			ledOn = 0b111111;     
 	}     else if (voltAsPercent == 7) {         
-		time = 70;      
-	}     else if (voltAsPercent == 8) {         
-		time = 80;    
-    }     else if (voltAsPercent == 9) {
-		time = 90;  
-	}     else if (voltAsPercent == 10) {         
-		time = 100;     
-	}     else if (voltAsPercent == 10) {         
-		time = 110;     
-	}     else if (voltAsPercent == 10) {         
-		time = 120;     
-	} 
-	return time; 
+		ledOn = 0b1111111;    
+    }     else if (voltAsPercent == 8) {
+		         ledOn = 0b11111111;     
+	}     else if (voltAsPercent == 9) {         
+		ledOn = 0b111111111;     
+	} else {
+        ledOn = 0b1111111111;
+    }
+	return ledOn; 
 } 
 
-// (from lab 3) check flag bit 15 to check that the channel has completed measuring voltage
-int checkADC(void){     
-		int bitCheck;     
-		bitCheck = adc_ptr->ch0 & 0x8000;     
-		return bitCheck; 
+int buttonsCheck(void ){
+	volatile int * button = (int*)KEY_BASE;
+
+	int buttonValue =* button;
+
+	return buttonValue;
 }
 
-int main(void) {   
-    // timer
-    myTimer->status = timer;
-    myTimer->control = timer + 1;
-    myTimer->loadL = timer + 2;
-    myTimer->loadH = timer + 3;
-    myTimer->readL = timer + 4;
-    myTimer->readH = timer + 5;
+int switchCheck(void){
+	volatile int * switchPointer = (int*)SW_BASE;
 
-    *(myTimer->loadL) = 0x4240;
-    *(myTimer->loadH) = 0xF;
-    *(myTimer->control) = 0b100;
-
-    // sets output bits (1) for 0-9 GPIO pins (will be for LED bank)
-	gpio_ptr->control = 0x3FF;     
-	int voltReading;     
-	int setTime;
-
-    start = 0;
-    time = 0;
-
-    while (1) {
-    // ----READS POTENTIOMETER----
-		// writes 1 to channel 0 to update channels (start ADC conversion)
-		adc_ptr->ch0 = 0x1;
-
-	    // checking bit 15 to see if the flag is set to 1
-		if (checkADC() == 0x8000) {   
-				voltReading = readVoltage();                 
-				setTime = voltageToPercent(voltReading);   
-                time += setTime;              
-				// gpio_ptr->data = ledBank;             
-	    } 
+	int switchValue = *switchPointer;
+	switchValue &= 1;
     
-        // ---- DISPLAY TIME ON 7-SEGMENT DISPLAY ----
-        Display(time);
+	return (switchValue);
+}
 
-        // --- READS BUTTONS ----
-        buttons = ReadButs();
-        // start
-        if (!start && buttons == 0b0001) {
-            start = 1;
-        }
-        // stop and reset
-        if (start && buttons == 0b0010) {
-            time = 0;
-            start = 0;
-        }
+int checkTime(void){
+	volatile int* status = (int*) TIMER_BASE;
+	int timCout = *status&1;
 
-        // decrement time when start is enabled
-        if ((*(myTimer->status) & 0x1) == 0b1 && start) {
+	if(timCout==1){
+		*status = 0; // 00 just write to the status 
+	}
 
-            // ** THIS WILL NEED TO BE CHANGED*****
-            // currently increments up and wraps around at 59secs
-            // we need it to wrap down from 0 sec to 59
-            if (time % 10000 == 5999) {
-                time += 4001; // when it reaches 59 sec
+	return (timCout);
+}
+ 
+void stopTimer(){
+	volatile int * control  = (int*)(TIMER_BASE + 4);
+	*control = 8; // 1000 
+}
+
+void startTimer(void){
+	volatile int * highPeriod = (int*)(TIMER_BASE + 12);
+	volatile int * lowPeriod = (int*)(TIMER_BASE + 8);
+	volatile int * control = (int*)(TIMER_BASE + 4);
+
+	*lowPeriod = 100 * 10000;
+
+	int val = 100 * 10000;
+	*highPeriod = val>>16;
+
+	*control = 6; // 0110
+}
+
+void showTimer(int val){
+	volatile int * displayOne = (int*)HEX3_HEX0_BASE;
+	volatile int * displayTwo = (int*)HEX5_HEX4_BASE;
+	unsigned char map[16] = {
+        0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F,
+        0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71};
+    
+    int minOne = (val/60000);
+    int minTwo = ((val%60000)/6000);
+    int secOne = ((val%6000)/1000);
+    int secTwo = ((val%1000)/100);
+    int msecOne = ((val%100)/10);
+    int msecTwo = (val%10);
+    
+    *displayOne = (map[msecTwo] + (map[msecOne]<<8) + (map[secTwo]<<16) + (map[secOne]<<24));
+    *displayTwo = (map[minTwo] + (map[minOne]<<8));	
+}
+
+int checkADC(void){     
+	int bitCheck;     
+	bitCheck = adc_ptr->ch0 & 1<16;     
+	return bitCheck; 
+}
+
+int main() {
+	int prevInstruction = 0;
+	int paused = 0;
+	int lapTime = 0;
+	int instruction = 0;
+	int countedTime = 0;
+
+    int totalTime = 0;
+
+    int weightRead = 0;
+
+    gpio_ptr->control = 0x3FF;
+    gpio_ptr->data = 0b0000000000;
+
+    adc_ptr->ch1 = 0x1;
+    
+    while (1) 
+	{
+		if(checkTime() == 1){
+            if (countedTime == 0){ //stop the count organically
+                int flash = 299;
+                while (flash > 1) {
+                    if (checkTime() == 1){
+                        flash--;
+                    }
+                    int flashStatus = 1;
+                    gpio_ptr->data = 0b1111111111;
+
+                    if ((flash & 50) == 0) {
+                        //printf("FLASH FLASH\n");
+                        //showTimer(99999);
+                        if (flashStatus == 1){
+                            gpio_ptr->data = 0b0000000000;
+                            //showTimer(1);
+                            flashStatus = 0;
+                        } else {
+                            gpio_ptr->data = 0b1111111111;
+                            //showTimer(0);
+                            flashStatus = 1;
+                        }
+                    }
+                }
+                stopTimer();
+                countedTime = totalTime;
+                paused = 0;
+            } else {
+                int ledOn = voltageToPercent(countedTime, totalTime);
+                gpio_ptr->data = ledOn; 
+                if(checkADC()) {
+                    weightRead = readVoltage(0);
+                }
+                if (switchCheck() == 1 || weightRead > (4096/2)){
+                    countedTime--;
+                }
             }
+		}
 
-            else {
-                time--;
+		instruction = buttonsCheck();
 
-                // LED BANK STUFF, ex:
-                // interval = time/100
-                // if (time == (time - interval)), then light up first LED
-                // if (time == 2*(time - interval)), then light up second LED ... so on...
-            }
-        }
-        *(myTimer->status) = 0x0;
-        *(myTimer->control) = 0b100;
-    }  
+		if(((instruction&1)==1) && ((prevInstruction&1)!=1) && (paused==0)){ //count down
+			startTimer();
+			paused = 1;   
+		}
+		else if(((instruction&2)==2) && ((prevInstruction&2)!=2) && (paused==1)){ //pause count down
+			stopTimer();
+			paused = 0;
+		}
+		else if(((instruction&4)==4) && ((prevInstruction&4)!=4)) { //add time to count down
+			countedTime += 100;
+            totalTime += 100;
+		}
+		else if(((instruction&8)==8) && ((prevInstruction&8)!=8)) { //reset count down
+			countedTime = 0;
+			lapTime = 0;
+
+			stopTimer();
+			paused = 0;
+            totalTime = 0;
+		}
+
+		prevInstruction = instruction;
+
+		showTimer(countedTime);
+    }
 }
